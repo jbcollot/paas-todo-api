bcb9f9ea9015cf543f446757d641e2bc9c04f61d
Collot Jean-Baptiste <jean-baptiste.collot@octo.com>
2023-03-16 14:04:05 +0100

    ajout de l'env

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
+const nom_env = process.env.NOM_ENV
 app.get("/", function (req, res) {
-  res.send(`Hello World!`)
+  res.send(`Hello World! Environnement de ${nom_env}`)
 })
-
 const port = process.env.PORT
 app.listen(port, function () {
   console.log(`ToDo API listening on port ${port}`)
